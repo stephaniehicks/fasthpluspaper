b2fb53c8f7f63315dd1d331f4773877a3baa7268
David Dyjack <daviddyjack@davids-air.lan>
2021-11-29 17:33:04 -0800

    performance plot

--- scripts/05-performance_plot.R
+++ scripts/05-performance_plot.R
@@ -1,32 +1,13 @@
 #heatmaps for 2 difficulties. include PCA plots to demonstrate the 
 set.seed(1234)
 library(RColorBrewer)
-library(viridis)
-library(leaflet)
 library(fasthplus)
 #cl1 <- '#8338EC' #Purple
 #cl2 <- '#06D6A0' #Teal
-colref <- palette.colors(palette = "Okabe-Ito")[2:8]
 
 set.seed(1234)
-n_vec <- seq(1000,10000,by=1000)
+n_vec <- c(500,seq(1000,10000,by=1000))
 m <- 500
-#q, number of boostraps
-#r, per-bootstrap sample size
-#q_vec <- c(1,3,5,10,20,30)
-#r_vec <- seq(10,100,by=10) #c(10,2,50,100)
-#q <- 30
-#r <- 100
-
-#full H+ calculation
-#n <- 1000
-#n1 <- n2 <- 500
-#m <- 500
-
-setups <-  list(
- list(b=c(0.5,0.5),m=c(-0.1,0.1),s=0.5),
- list(b=c(0.25,0.25,0.25,0.25),m=c(-0.2,-0.1,0.1,0.2),s=0.5)
-)
 
 calcf <- function(d,l,r){
   stmp <- table(l)/length(l)
@@ -42,63 +23,69 @@ calcf <- function(d,l,r){
   dbtmp <- distmp[ibtmp]
   sptmp <- sum(sapply(dwtmp, function(x) sum(x>dbtmp)))
   hptmp <- sptmp / (as.numeric(length(dwtmp))*as.numeric(length(dbtmp)))
-  return(hptmp) 
+ return(hptmp) 
 }
 
 
-dat <- lapply(n_vec, function(n) {
-  dat <- sapply(1:n, function(i) rnorm(n=m,mean=x$m[j],sd=x$s))) 
+res <- sapply(n_vec, function(n) {
+  dat <- t(sapply(1:n, function(i) rnorm(n=m,mean=0,sd=1)))
+  lab <- c(rep(0,n/2),rep(1,n/2))
   #time distance calculation
-  #time adjacency calculation
-  #time s+ calculation
+  ptm <- proc.time()
+  dis <- dist(dat)
+  t_dis <- unname(proc.time() - ptm)[3]
   #time hpe calculation
+  ptm <- proc.time()
+  hpe <- hpe(D=dis,L=lab,p=251)
+  t_hpe <- unname(proc.time() - ptm)[3]
+  #extra dis calcs (untimed)
+  dis <- as.matrix(dis)
+  dis <- dis[upper.tri(dis)]
+  #time adjacency calculation
+  ptm <- proc.time()
+  ind <- sapply(lab, function(x) x==lab)
+  ind <- ind[upper.tri(ind)]
+  iw <- which(ind)
+  ib <- which(!ind)
+  t_adj <- unname(proc.time() - ptm)[3]
+  #time s+ calculation
+  if(n < 2000){
+    ptm <- proc.time()
+    sp <- sum(sapply(dis[iw], function(x) sum(x>dis[ib])))
+    t_spl <- unname(proc.time() - ptm)[3]
+  } else {
+    t_spl <- NA
+  }
   #time hpb calculation (fixed n+r)
+  ptm <- proc.time()
+  hpb <- mean(replicate(30,calcf(dat,lab,100),T)) 
+  t_hpb <- unname(proc.time() - ptm)[3]
+  c(t_dis,t_adj,t_spl,t_hpe,t_hpb)
 })
 
-#dat <- lapply(setups, function(x) {
-#  len <- 1:length(x$b)
-#  z <- lapply(len, function(j) sapply(1:round(x$b[j]*n), function(i) rnorm(n=m,mean=x$m[j],sd=x$s)))
-#  z <- t(do.call(cbind,z))
-#  lab <- as.vector(sapply(len, function(j) rep(j,round(x$b[j]*n))))
-#  col <- sapply(lab, function(j) colref[j])
-#  pc <- prcomp(z)$x[,1:2]
-#  dis <- dist(z)
-#  hp <- hpe(D=dis,L=lab,p=10001)$h
-#  hpb <- sapply(r_vec, function(r) {
-#    sapply(q_vec, function(q) {
-#      mean(replicate(q,calcf(z,lab,r),T))
-#    })
-#  })
-#  fin <- list(p=pc, h=hp, hb=hpb, res=t(abs(hp-hpb)), c=col,b=x$b)
-#  return(fin)
-#})
-#nr <- length(r_vec)
-#nc <- length(q_vec)
-#gres <- dat[[1]]$res
-#hres <- dat[[2]]$res
-#plotlocs <- rbind(
-#  c(0.01,0.51,0.35,0.85), #left heatmap
-#  c(0.49,0.99,0.35,0.85), #right heatmap
-#  c(0.30,0.70,0.90,0.97), #top colorbar
-#  c(0.05,0.48,0.02,0.32), #bottomleft pcaplot
-#  c(0.53,0.98,0.02,0.32)  #bottomright pcaplot
-#)
-#zlim <- c(0,max(rbind(gres,hres)))
-#nlevs <- 100
-#levs <- seq(0,1,length.out=nlevs)
-#col_pal <- viridis(nlevs-1)
-#col_pal_fxn <- colorNumeric(col_pal, domain=zlim, na.color = "#808080",alpha = FALSE,reverse = FALSE)
-#xlim <- c(-0.5,nc+1)
-#ylim <-c(-0.5,nr+0.4)
-#zleglocs <- c(0.01,0.50,0.99)
-#zleglabs <- formatC(seq(zlim[1],zlim[2],length.out=3),format='f',digits=2)
-#txtcut <- 0.05
-#xlabs <- as.character(q_vec)
-#ylabs <- as.character(r_vec)
-#yvals <- sapply(1:nr, function(i) c(i-0.5,i+0.5))
+colref <- c('#000000','#E69F00','#009E73','#0072B2','#CC79A7') #palette.colors(palette = "Okabe-Ito")[2:6]
+pchref <- 21:25
 
-pdf("05-bootstrap_accuracy.pdf",width=6,height=4)
-par(mfrow=c(1,1),mar=c(1,1,1,1))
+ylim <- c(-50,1.05*max(res,na.rm=T))
+yaxl <- pretty(c(0,max(res,na.rm=T)),n=3)
+yaxt <- formatC(yaxl,digits=0,format='f')
+
+xlim <- c(0,10500)
+xaxl <- n_vec
+xaxt <- as.character(n_vec)
+
+pdf("05-performance_plot.pdf",width=6,height=4)
+  par(mfrow=c(1,1),mar=c(3.1,3.1,0.6,0.6))
+  plot(x=1,y=1,type='n',xaxs = "i",yaxs = "i",xlab='',ylab='',xaxt='n',yaxt='n',xlim=xlim,ylim=ylim)
+  for(i in 1:nrow(res)){
+    lines(x=n_vec,y=res[i,],col=colref[i],lwd=2.0,type='b',pch=pchref[i],cex=1.5,lty=i)
+  }
+  mtext(side=2,text='computation time (s)',line=1.5,las=3,cex=1.3)
+  mtext(side=1,text='number of observations',line=1.5,las=1,cex=1.3)
+  axis(side=1,labels=xaxt,at=xaxl,cex.axis=0.5,las=1,mgp=c(1.0, .1, 0))
+  axis(side=2,labels=yaxt,at=yaxl,cex.axis=0.7,las=3,mgp=c(3.0, .4, 0))
+  legend(x='topright',legend=c('Dissimilarity','Adjacency','S+ (Dw-Db comparisons)','HPE (pre-calculated D)','HPB (r=30,q=100)'),
+    lty=1:5,pch=pchref,col=colref,lwd=2.0,bty='n')
 
 dev.off()
 
