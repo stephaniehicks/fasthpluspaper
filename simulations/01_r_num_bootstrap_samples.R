525e9310c8a70b631808f056b6dc987f01c3dce8
Sowmya Parthiban <sparthi1@jh.edu>
2022-04-01 19:06:54 -0400

    create script outline for hbp across different r

---
+++ simulations/01_r_num_bootstrap_samples.R
@@ -0,0 +1,17 @@
+#load relevant packages
+library(here)
+
+
+#code for simulating data 
+#https://github.com/stephaniehicks/benchmark-hdf5-clustering/blob/2020-05-07-freeze/scripts/simulate_gauss_mix_k.R
+
+#params to fix
+# k 
+# means for each group
+# sd for each group
+#proportion belonging to each group
+#num observations and num genes
+
+
+#Explore what happens when you apply hpb()  varying the number of bootstrap samples (`r).
+
